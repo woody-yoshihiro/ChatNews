961b8e4357cd37ec1a33c00f055610bc09bb3074
woody-yoshihiro <woody19810930@gmail.com>
2020-05-27 13:13:21 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "home#index"
+  get "home/index"
+
   resources :user_clips
   
-  resources :clips , only: [:index, :show] do
+  resources :clips, only: [:index, :show] do
     resources :opinions, shallow: true
   end
-
-  devise_for :users
-  get "home/index"
-  root "home#index"
 end
